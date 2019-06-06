b72dbb8513aef981516095b7095e1334cb4158d7
zhangjinpeng1987 <zhangjinpeng@pingcap.com>
2019-06-06 10:47:16 +0800

    inline put_msg
    Signed-off-by: zhangjinpeng1987 <zhangjinpeng@pingcap.com>

--- components/engine/src/mutable.rs
+++ components/engine/src/mutable.rs
@@ -4,6 +4,7 @@ use crate::rocks::{CFHandle, Writable};
 use crate::Result;
 
 pub trait Mutable: Writable {
+    #[inline]
     fn put_msg<M: protobuf::Message>(&self, key: &[u8], m: &M) -> Result<()> {
         let value = m.write_to_bytes()?;
         self.put(key, &value)?;
@@ -11,12 +12,14 @@ pub trait Mutable: Writable {
     }
 
     // TODO: change CFHandle to str.
+    #[inline]
     fn put_msg_cf<M: protobuf::Message>(&self, cf: &CFHandle, key: &[u8], m: &M) -> Result<()> {
         let value = m.write_to_bytes()?;
         self.put_cf(cf, key, &value)?;
         Ok(())
     }
 
+    #[inline]
     fn del(&self, key: &[u8]) -> Result<()> {
         self.delete(key)?;
         Ok(())
